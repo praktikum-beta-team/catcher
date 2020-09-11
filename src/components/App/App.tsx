3913e4a4a008526f8c545e5f39614efd5ba6e8ca
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-09-11 17:04:43 +0300

    Добавляет предохранители к страницам

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,14 +5,31 @@ import { Signin } from "components/Signin";
 import { Signup } from "components/Signup";
 import { Settings } from "components/Settings";
 import { Leaderboard } from "components/Leaderboard";
+import { ErrorBoundary } from "components/ErrorBoundary";
 
 export const App: FC = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Signin} />
-      <Route exact path="/signup" component={Signup} />
-      <Route exact path="/settings" component={Settings} />
-      <Route exact path="/leaderboard" component={Leaderboard} />
+      <Route exact path="/">
+        <ErrorBoundary>
+          <Signin />
+        </ErrorBoundary>
+      </Route>
+      <Route exact path="/signup">
+        <ErrorBoundary>
+          <Signup />
+        </ErrorBoundary>
+      </Route>
+      <Route exact path="/settings">
+        <ErrorBoundary>
+          <Settings />
+        </ErrorBoundary>
+      </Route>
+      <Route exact path="/leaderboard">
+        <ErrorBoundary>
+          <Leaderboard />
+        </ErrorBoundary>
+      </Route>
     </Switch>
   </BrowserRouter>
 );
