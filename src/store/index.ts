773f3499f870a65e813ff5d542e3219f93da271a
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-09-19 09:36:19 +0300

    Добавляет Redux DevTools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
-const rootReducer = combineReducers([]);
+import { authReducer as auth } from "services/auth/slice";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+const rootReducer = combineReducers({
+  auth,
+});
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export type RootState = ReturnType<typeof store.getState>;
