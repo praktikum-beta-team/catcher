ef7efa95ee91b945e8ebe24ee1c83204c637523e
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-09-21 12:25:46 +0300

    Исправляет выход из аккаунта

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -1,16 +1,19 @@
 import React, { FC, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 import { logoutRequest } from "services/auth/actions";
 import { ROUTES } from "constants/routes";
+import { isAuthenticatedSelector } from "services/auth/selectors";
+import { Loading } from "components/UI";
 
 export const Logout: FC = () => {
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector(isAuthenticatedSelector);
 
   useEffect(() => {
     dispatch(logoutRequest());
   }, []);
 
-  return <Redirect to={ROUTES.SIGNIN} />;
+  return isAuthenticated ? <Loading /> : <Redirect to={ROUTES.SIGNIN} />;
 };
