cddef30bde3f7f15a4982ad27e288f68afffcebd
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-10-06 23:05:55 +0300

    Добавляет игру в компонент

--- src/views/GameScreen/GameScreen.tsx
+++ src/views/GameScreen/GameScreen.tsx
@@ -1,15 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useRef } from "react";
 
+import { Game } from 'services/game'
 import { cn } from "helpers/classname";
 
 import "./GameScreen.css";
 
 const b_ = cn("game-screen");
 
-export const GameScreen: FC = () => (
-  <div className={b_()}>
-    <span role="img" aria-label="">
-      😼
-    </span>
-  </div>
-);
+export const GameScreen: FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d')
+
+    if (ctx) {
+      const game = new Game(ctx)
+      game.render()
+    }
+
+    return () => {
+      console.log('unmount')
+    }
+  }, [])
+  
+  return (
+      <div className={b_()}>
+        <canvas ref={canvasRef} width={320} height={480} />
+      </div>
+    );
+};
