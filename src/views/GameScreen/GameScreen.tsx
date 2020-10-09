7cb1190bae80a417b582ca69c7cc7c67807d8967
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-10-09 12:42:31 +0300

    Рефакторинг

--- src/views/GameScreen/GameScreen.tsx
+++ src/views/GameScreen/GameScreen.tsx
@@ -1,18 +1,21 @@
 import React, { FC, useEffect, useRef } from "react";
 
-import { Game } from 'services/game'
+import { Game } from "services/game";
 import { cn } from "helpers/classname";
 
 import "./GameScreen.css";
 
-const b_ = cn("game-screen");
+const WIDTH = 320;
+const HEIGHT = 480;
+
+const cnGameScreen = cn("game-screen");
 
 export const GameScreen: FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     let game: Game;
-    const ctx = canvasRef.current?.getContext('2d')
+    const ctx = canvasRef.current?.getContext("2d");
 
     if (ctx) {
       game = new Game(ctx);
@@ -20,13 +23,11 @@ export const GameScreen: FC = () => {
     }
 
     return () => game && game.destroy();
-  }, [])
-  
+  }, []);
+
   return (
-    <div className={b_()}>
-      <canvas ref={canvasRef} width={320} height={480}>
-        Canvas disabled
-      </canvas>
+    <div className={cnGameScreen()}>
+      <canvas ref={canvasRef} width={WIDTH} height={HEIGHT} />
     </div>
   );
 };
