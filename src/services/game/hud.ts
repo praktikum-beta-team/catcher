9307fc0cb23b843022ac673f338a8992c5a1d6f1
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-10-09 13:57:48 +0300

    Отображает иконки вместо числа жизней

--- src/services/game/hud.ts
+++ src/services/game/hud.ts
@@ -12,13 +12,12 @@ export class HUD {
     const { width } = ctx.canvas;
 
     ctx.textBaseline = "top";
-
-    ctx.font = "24px sans-serif";
-    ctx.fillStyle = "white";
-    ctx.textAlign = "start";
-    ctx.fillText(`${lives}`, 0, 0);
-
     ctx.font = "18px sans-serif";
+
+    ctx.fillStyle = "red";
+    ctx.textAlign = "start";
+    ctx.fillText("❤".repeat(lives), 0, 0);
+
     ctx.fillStyle = "grey";
     ctx.textAlign = "end";
     ctx.fillText(`${score}`, width, 0);
