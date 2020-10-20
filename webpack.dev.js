d4c9a45798b641facc4e7f0e583a45b1447c66d9
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-10-20 10:28:12 +0300

    Включает Live Reloading

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,18 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
+  target: "web",
   devtool: "inline-source-map",
   devServer: {
-    historyApiFallback: true,
+    contentBase: path.join(__dirname, "dist"),
+    watchContentBase: true,
     open: true,
+    historyApiFallback: true,
+    port: 8000,
   },
   module: {
     rules: [
