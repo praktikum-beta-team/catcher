91745355a87079eea9256b9109bdc483535e2f9d
Aleksandr Yakovlev <Aleksandr.Yakovlev@live.com>
2020-12-10 20:13:58 +0300

    добавляет PointerLock API для управления курсом

--- src/app/lib/game/objects/bucket.ts
+++ src/app/lib/game/objects/bucket.ts
@@ -18,21 +18,22 @@ export class Bucket implements IGameObject {
     this.y = ctx.canvas.height - this.height;
   }
 
-  draw() {
+  draw(): void {
     const { ctx, width, height } = this;
 
     ctx.fillStyle = "white";
     ctx.fillRect(this.x, this.y, width, height);
   }
 
-  move(left = false) {
+  move(position: { direction?: "left" | "right"; dx?: number }): void {
+    const { direction, dx = 0 } = position;
     const { ctx, width } = this;
     const { width: canvasWidth } = ctx.canvas;
-
-    if (left && this.x > 0) {
-      this.x -= SPEED;
-    } else if (this.x < canvasWidth - width) {
-      this.x += SPEED;
+    const ddx = dx === 0 ? 1 : Math.abs(dx);
+    if ((direction === "left" || dx < 0) && this.x > 0) {
+      this.x -= ddx * SPEED;
+    } else if ((position.direction === "right" || dx > 0) && this.x < canvasWidth - width) {
+      this.x += ddx * SPEED;
     }
   }
 }
