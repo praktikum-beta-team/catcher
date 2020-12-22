076137d0cadf33d1bcb8030237a1db7f839ebaad
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2020-12-22 20:32:57 +0300

    Проксирование запросов к бэкенду

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import open from "open";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 import { ROUTES } from "app/constants/routes";
 import ssr from "server/middlewares/ssr";
@@ -9,13 +10,19 @@ import { yandexOAuth } from "server/middlewares/yandexOAuth";
 import TEXT from "server/constants/text";
 import { IS_DEVELOPMENT, PORT } from "config/vars";
 
+const proxyMiddleware = createProxyMiddleware({
+  target: "https://ya-praktikum.tech",
+  changeOrigin: true,
+});
+
 const app = express();
 
 app
-  .use(morgan("tiny"))
   .use(express.static("./dist"))
+  .use("/api", proxyMiddleware)
   .get(ROUTES.OAUTH, yandexOAuth)
   .use(ssr)
+  .use(morgan("tiny"))
   .listen(PORT, () => {
     console.log(`${TEXT.SERVER_RUNNING_MESSAGE} ${PORT}`);
     if (IS_DEVELOPMENT) open(`http://localhost:${PORT}`);
