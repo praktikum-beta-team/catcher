f74d7f3ea844c51925830fc506ba8ffe6b6565a2
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-01-07 12:10:30 +0300

    Выделение активных ссылок в меню

--- src/components/UI/Nav/Nav.tsx
+++ src/components/UI/Nav/Nav.tsx
@@ -1,5 +1,5 @@
 import React, { FC, HTMLProps } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import { ROUTES } from "constants/routes";
 import { cn } from "helpers/classname";
@@ -12,9 +12,22 @@ export const Nav: FC<HTMLProps<HTMLDivElement>> = ({ className, ...restNavProps 
   <nav className={b_(null, [className])} {...restNavProps}>
     <ul className={b_("list")}>
       <li className={b_("item")}>
-        <Link className={b_("link")} to={ROUTES.LEADERBOARD}>
+        <NavLink
+          className={b_("link")}
+          to={ROUTES.GAME}
+          activeClassName={b_("link", { active: true })}
+        >
+          Игра
+        </NavLink>
+      </li>
+      <li className={b_("item")}>
+        <NavLink
+          className={b_("link")}
+          to={ROUTES.LEADERBOARD}
+          activeClassName={b_("link", { active: true })}
+        >
           Список лидеров
-        </Link>
+        </NavLink>
       </li>
     </ul>
   </nav>
