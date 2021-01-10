132d039d7c53350e0483c864a973f32e8a1deb98
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-01-10 23:17:20 +0300

    Рефакторинг серверной части

--- server/app.ts
+++ server/app.ts
@@ -1,25 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-import fs from "fs";
-import https from "https";
-import path from "path";
 
+import { port } from "config/vars";
 import { ROUTES } from "constants/routes";
 import ssr from "server/middlewares/ssr";
 import { yandexOAuth } from "server/middlewares/yandexOAuth";
 import { apiProxy } from "server/middlewares/apiProxy";
 import { fetchData } from "server/middlewares/fetchData";
 
-import { TEXT } from "constants/text";
-import { port } from "config/vars";
 import { baseURL } from "constants/api";
 
-const key = fs.readFileSync(path.resolve("./config/ssl/localhost.key"));
-const cert = fs.readFileSync(path.resolve("./config/ssl/localhost.crt"));
-
 const app = express();
-const server = https.createServer({ key, cert }, app);
 
 const middlewares = [morgan("tiny"), cookieParser(), fetchData, ...ssr];
 
@@ -29,11 +21,13 @@ app
   .get(ROUTES.OAUTH, yandexOAuth)
   .use(middlewares);
 
-server.listen(port, () => {
-  console.log(`${TEXT.SERVER.SERVER_RUNNING_MESSAGE} ${port}`);
-});
-
 /**
  * TODO: можно попробовать на бою генерировать сертификат с помощью
  * https://git.coolaj86.com/coolaj86/greenlock-express.js
  */
+
+if (require.main === module) {
+  app.listen(port);
+}
+
+module.exports = app;
--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,18 @@
-require("./app");
+import { readFileSync } from "fs";
+import { resolve } from "path";
+import https from "https";
+
+import { port } from "config/vars";
+import { TEXT } from "constants/text";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const app = require("./app");
+
+const key = readFileSync(resolve("./config/ssl/localhost.key"));
+const cert = readFileSync(resolve("./config/ssl/localhost.crt"));
+
+const server = https.createServer({ key, cert }, app);
+
+server.listen(port, () => {
+  console.log(`${TEXT.SERVER.SERVER_RUNNING_MESSAGE} ${port}`);
+});
