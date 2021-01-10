b305ae90b5407bf5a6bf5eb6219e40e467cc3fab
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-01-10 14:53:02 +0300

    Обновление верстки страницы входа

--- src/views/Signin/Signin.tsx
+++ src/views/Signin/Signin.tsx
@@ -2,14 +2,15 @@ import React, { FC } from "react";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
-import { TEXT } from "constants/text";
 import { authSelectors } from "store/auth";
 import { cn } from "helpers/classname";
-import { Modal } from "components/UI";
 import { SigninForm } from "components/SigninForm";
+import { Modal } from "components/UI";
 import { ROUTES } from "constants/routes";
+import { TEXT } from "constants/text";
 
 import "./Signin.css";
+import * as PromoImage from "./Signin__PromoImage.svg";
 
 const b_ = cn("signin");
 
@@ -19,9 +20,16 @@ export const Signin: FC = () => {
     <Redirect to={ROUTES.GAME} />
   ) : (
     <div className={b_()}>
-      <Modal title={TEXT.SIGNIN.TITLE}>
-        <SigninForm />
-      </Modal>
+      <div className={b_("layout")}>
+        <aside className={b_("section")}>
+          <img className={b_("promo-image")} src={PromoImage.default} alt="" />
+        </aside>
+        <main className={b_("section")}>
+          <Modal title={TEXT.SIGNIN.TITLE}>
+            <SigninForm />
+          </Modal>
+        </main>
+      </div>
     </div>
   );
 };
