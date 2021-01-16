fe7b5a51158167bab0aa226e9c1344fcdbaefdab
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-01-16 15:48:45 +0300

    Исправление Яндекс.oAuth redirect url

--- src/constants/yandexOAuth.ts
+++ src/constants/yandexOAuth.ts
@@ -9,7 +9,8 @@ const { baseDomain, port } = settings;
  */
 export const CLIENT_ID = "3bd97dc79c8a4398aecda914461a2c2a";
 export const CLIENT_SECRET = "caee736bc0d2469f9ce683b424293f22";
-const redirectUri = `https://${baseDomain}${environment === "development" && `:${port}`}${
-  ROUTES.OAUTH
-}`;
+
+const redirectUriPort = environment === "development" ? `:${port}` : "";
+const redirectUri = `https://${baseDomain}${redirectUriPort}${ROUTES.OAUTH}`;
+
 export const AUTH_URL = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${redirectUri}`;
