f313ed69ffe6fcbe9ac595d5e2a6341883f1a731
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-01-16 15:44:29 +0300

    Исправление перезаписи домена кук

--- server/middlewares/apiProxy/apiProxy.ts
+++ server/middlewares/apiProxy/apiProxy.ts
@@ -1,7 +1,11 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
 
+import { settings } from "config/settings";
+
+const { apiBaseUrl, baseDomain } = settings;
+
 export const apiProxy = createProxyMiddleware({
-  target: "https://ya-praktikum.tech",
+  target: apiBaseUrl,
   changeOrigin: true,
-  cookieDomainRewrite: "localhost",
+  cookieDomainRewrite: baseDomain,
 });
